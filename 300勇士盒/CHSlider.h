95aaacfeea9101dcd623569ff9c193942474d29b
ChenHao <info@mrchenhao.com>
2014-10-04 02:34:02 +0800

    add cocoa pod and AFNnetworking

---
+++ 300勇士盒/CHSlider.h
@@ -0,0 +1,13 @@
+//
+//  CHSlider.h
+//  300勇士盒
+//
+//  Created by ChenHao on 10/4/14.
+//  Copyright (c) 2014 xxTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHSlider : UIViewController
+
+@end
---
+++ 300勇士盒/CHSlider.m
@@ -0,0 +1,29 @@
+//
+//  CHSlider.m
+//  300勇士盒
+//
+//  Created by ChenHao on 10/4/14.
+//  Copyright (c) 2014 xxTeam. All rights reserved.
+//
+
+#import "CHSlider.h"
+
+@interface CHSlider ()
+
+@end
+
+@implementation CHSlider
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
